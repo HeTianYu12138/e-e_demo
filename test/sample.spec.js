03a0c04bb39322fe1827995b7b662047c74c0d58
HeTianYu12138 <895562423@qq.com>
2020-04-01 19:14:04 +0800

    加入删除和获取功能

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -11,7 +11,7 @@ describe('add todo', function () {
     });
 
     it('should have correct title', async function() {
-        expect(await page.title()).to.eql('Puppeteer Demo');
+        expect(await page.title()).to.eql('Koa • Todo');
     })
     it('should new todo correct', async function() {
       await page.click('#new-todo', {delay: 500});
@@ -22,4 +22,42 @@ describe('add todo', function () {
       expect(expectInputContent).to.eql('new todo item');
     }) 
 
+  });
+  describe('delete todo', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
+    });
+   it('should delete dodo correctly',async function(){
+    //  let todoList = await page.waitFor('#todo-list');
+    let length = await page.$$eval('#todo-list li',list=> list.length);
+     console.log(length)
+     await page.evaluate(()=> {
+      document.querySelector('.destroy').click()
+    });
+    await page.waitFor('#todo-list');
+    let length2 = await page.$$eval('#todo-list li',list=>list.length);
+     expect(length2).to.eql(length-1);
+   })
+    after (async function () {
+      await page.close();
+    });
+  });
+  describe('get todo', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
+    });
+    it('should get dodo correctly',async function(){
+      //  let todoList = await page.waitFor('#todo-list');
+      let list = await page.$$('#todo-list li');
+       expect(list).to.have.property('length');
+     })
+    after (async function () {
+      await page.close();
+    });
   });
